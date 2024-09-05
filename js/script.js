b2cc7ddb18c4c17ab0af206550ada24ea2f5db5b
karlos <carlos.kalifa21@gmail.com>
2024-09-05 01:07:00 -0500

    creacion del formulario por terminar validacion

--- js/script.js
+++ js/script.js
@@ -15,3 +15,55 @@ document.addEventListener("DOMContentLoaded", function() {
     setInterval(showNextImage, interval);
 });
 
+document.getElementById('form').addEventListener('submit', function(){
+
+    
+
+    const nombre = document.getElementById('nombre').value.trim();
+    const correo = document.getElementById('correo').value.trim();
+    const material = document.getElementById('material').value.trim();
+    const elemento = document.getElementById('numeroelemento').value.trim();
+    
+     // Mensaje de error
+  let errorMessage = '';
+
+  // Validaciones
+  if (nombre === '') {
+    errorMessage += 'El nombre no puede estar vacío. ';
+  }
+  
+  if (correo === '') {
+    errorMessage += 'El correo electrónico no puede estar vacío. ';
+  } 
+  if (material === '') {
+    errorMessage += 'El material no puede estar vacío. ';
+  }if (elemento === '') {
+    errorMessage += 'El N° elemento no puede estar vacío. ';
+  } else if (isNaN(elemento) || elemento <= 0) {
+    errorMessage += 'El elemento debe ser superior a 000 positivo. ';
+  }
+
+  // Muestra errores o envía el formulario
+  if (errorMessage) {
+    document.getElementById('error-message').textContent = errorMessage;
+  } else {
+    // Aquí puedes enviar el formulario o realizar otra acción
+    alert('Formulario válido y enviado.');
+    // Enviar el formulario
+    // this.submit();
+  }
+
+
+// Función para validar correos electrónicos
+function validateEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
+ 
+console.log(`nombre es : ${nombre}`);
+console.log(`correo es : ${correo}`);
+console.log(`material es : ${material}`);
+console.log(`N° elemento es : ${elemento}`);
+});
+  
+
